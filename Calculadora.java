facdde4c84d564a2d8752230496655ed860a8c7a
giselaAS <g154sousa@gmail.com>
2023-02-28 23:46:33 -0300

    Configurando GitHub Actions

---
+++ Calculadora.java
@@ -0,0 +1,10 @@
+public class Calculadora {
+
+  public int soma(int x, int y) {
+    return x + y;
+  }
+
+  public int subtrai(int x, int y) {
+    return x - y;
+  }
+}
---
+++ CalculadoraTest.java
@@ -0,0 +1,16 @@
+public class CalculadoraTest {
+
+  @Test
+  public void testeSoma1() {
+    Calculadora calc = new Calculadora();
+    int resultadoEsperado = 5;
+    int resultadoRetornado = calc.soma(2,3);
+    assertEquals(resultadoEsperado, resultadoRetornado);
+  }
+
+  @Test
+  public void testeSoma2() {
+    Calculadora calc = new Calculadora();
+    assertEquals(10, calc.soma(4,6));
+  }
+}
